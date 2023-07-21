9e77c17e824def753a8b0d0093c92768b10e6b23
cheater <504083161@qq.com>
2023-07-21 14:50:58 +0800

    first

---
+++ FG_MOYU/src/main/java/com/example/fg_moyu/FgMoyuApplication.java
@@ -0,0 +1,13 @@
+package com.example.fg_moyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgMoyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FgMoyuApplication.class, args);
+    }
+
+}
